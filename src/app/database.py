d4d48bb610e24635d396da074c5a07e934fa061b
mhayescs19 <mhayescs19@gmail.com>
2024-11-12 22:53:06 -0800

    update sqlite location

--- src/app/database.py
+++ src/app/database.py
@@ -3,11 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Replace with your database URL if needed
+SQLALCHEMY_DATABASE_URL = "sqlite:///../test.db"  # Replace with your database URL if needed
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # Only for SQLite
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
