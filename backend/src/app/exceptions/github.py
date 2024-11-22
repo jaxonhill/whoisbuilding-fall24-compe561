176503a3c4a35f5232b253200a367a096a660ceb
mhayescs19 <mhayescs19@gmail.com>
2024-11-21 21:51:45 -0800

    create github username exception

---
+++ backend/src/app/exceptions/github.py
@@ -0,0 +1,8 @@
+class GitHubUsernameException(Exception):
+    """Exception raised for a github username that does not exist
+    """
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return f"{self.message}"
