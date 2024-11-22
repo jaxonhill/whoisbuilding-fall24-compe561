ce65ef8f1e71a06236691659503be8ee84c16ee3
mhayescs19 <mhayescs19@gmail.com>
2024-11-21 23:27:19 -0800

    added dtos for github contributions

--- backend/src/app/dtos.py
+++ backend/src/app/dtos.py
@@ -1,9 +1,19 @@
 from pydantic import BaseModel
-
-## data transfer object wrappers
+from datetime import datetime
+## data transfer object wrappers for data no represented in database
 
 class GitHubRepository(BaseModel):
     """Repository object
     """
     name: str
     link: str
+
+class GitHubUsername(BaseModel):
+    username: str
+
+class GitHubContributionResponse(GitHubUsername):
+    contributions: int
+
+class GitHubContributionSummary(GitHubContributionResponse):
+    from_date: datetime
+    to_date: datetime
