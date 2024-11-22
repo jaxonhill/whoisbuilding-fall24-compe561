11d67272e664ae250e1cf8f3107525b9c702f5c2
jaxonhill <jaxoncharleshill@gmail.com>
2024-11-21 21:02:29 -0800

    Add logo for Navbar

---
+++ frontend/src/features/navbar/components/logo.js
@@ -0,0 +1,16 @@
+import { Hammer } from "lucide-react";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the font weights you need
+});
+
+export default function Logo() {
+  return (
+    <div className="flex gap-1 items-center">
+        <Hammer className="h-8 w-8" />
+        <p className={`${poppins.className} font-medium text-xl`}>whoisbuilding</p>
+    </div>
+  )
+}
