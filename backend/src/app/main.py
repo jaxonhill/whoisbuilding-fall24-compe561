aba379de8125cc262f7a4d70a282291238f46987
mhayescs19 <mhayescs19@gmail.com>
2024-11-21 23:28:15 -0800

    add tags for api routes

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .routers import users
 from .auth import router as auth_router
+from app.routers.github import router as github_router
 from .database import engine
 from .models import Base
 import logging
@@ -10,10 +11,11 @@ app = FastAPI()
 # Create tables if they don't exist
 Base.metadata.create_all(bind=engine)
 
-# Include user router
-app.include_router(users.router)
-## include auth router
-app.include_router(auth_router)
+# add all routers to app
+app.include_router(users.router,tags=["Users"])
+app.include_router(auth_router, tags=["Auth"])
+app.include_router(github_router,prefix="/github", tags=["GitHub"])
+
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
