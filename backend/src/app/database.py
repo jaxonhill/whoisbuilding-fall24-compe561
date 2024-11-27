e5803a554f2e0665357bdc84c9062e65414da4e3
mhayescs19 <mhayescs19@gmail.com>
2024-11-26 22:37:14 -0800

    update db connection url

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Replace with your database URL if needed
+SQLALCHEMY_DATABASE_URL = "postgresql://root:pass123@localhost:5432/whoisbuilding-dev"  # Replace with your database URL if needed
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # Only for SQLite
+    SQLALCHEMY_DATABASE_URL  # Only for SQLite
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
