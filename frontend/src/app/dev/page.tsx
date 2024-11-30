13732e7be8bb85fcfec79f4d1e45576ed02ebd2b
mhayescs19 <mhayescs19@gmail.com>
2024-11-29 22:09:22 -0800

    added development page to test new components

---
+++ frontend/src/app/dev/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import GitHubChart from "@/components/github-chart";
+
+export default function Dev() {
+  return (
+    <>
+      <GitHubChart gitHubUsername="mhayescs19"></GitHubChart>
+    </>
+  );
+}
