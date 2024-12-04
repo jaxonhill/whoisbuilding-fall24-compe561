cd47832b5d18bbdd4cbe204675cdb7fd1cbaef53
Jaxon Hill <jaxoncharleshill@gmail.com>
2024-12-03 18:12:43 -0800

    Edit navbar to be dynamic depending on user

--- frontend/src/features/navbar/components/navbar.tsx
+++ frontend/src/features/navbar/components/navbar.tsx
@@ -1,95 +1,58 @@
 "use client";
 
+import { User } from "@/types/db-types";
+import { NAVBAR_LINKS } from "../utils/utils";
 import Logo from "./logo";
+import NavbarLink from "./navbar-link";
+import { create_profile_page_link } from "@/utils/utils";
+import { Plus } from "lucide-react";
 import { useAuth } from "@/features/auth/context/auth-context";
 
 export default function Navbar() {
-    const { user, login, logout } = useAuth();
+    const { user, logout } = useAuth();
 
-    return (
-        <nav className="h-20 flex justify-between items-center text-base mb-8">
-            <Logo />
-            <div className="flex gap-4 items-center">
-                {user ? (
-                    <>
-                        <span className="text-slate-700">{user.name}</span>
-                        <button 
-                            onClick={logout}
-                            className="h-10 px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600"
-                        >
-                            Log Out
-                        </button>
-                    </>
-                ) : (
-                    <>
-                        <a
-							href="/login"
-							className="flex justify-center items-center h-10 px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600"
-						>
-                            Log In
-                        </a>
-                        <a
-                            href="/signup"
-                            className="text-blue-700 hover:underline hover:cursor-pointer"
-                        >
-                            Sign Up
-                        </a>
-                    </>
-                )}
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="h-24 flex justify-between items-center">
+			<Logo />
+			<div className="flex gap-8 items-center">
+				{NAVBAR_LINKS.map((navbar_link) => {
+					return <NavbarLink key={navbar_link.label} navbar_link={navbar_link} />
+				})}
+				{user ? <LoggedInSection user={user} onLogoutButtonClick={logout} /> : <LoggedOutSection />}
+			</div>
+		</nav>
+	);
 }
 
-// =======
-// import { User } from "@/types/common-types";
-// import { NAVBAR_LINKS } from "../utils/utils";
-// import Logo from "./logo";
-// import NavbarLink from "./navbar-link";
-// import { create_username_link, CURRENT_TEST_USER } from "@/utils/utils";
-// import { Plus } from "lucide-react";
+interface LoggedInSectionProps {
+	user: User;
+    onLogoutButtonClick: () => void;
+}
 
-// export default function Navbar() {
-// 	return (
-// 		<nav className="h-24 flex justify-between items-center">
-// 			<Logo />
-// 			<div className="flex gap-8 items-center">
-// 				{NAVBAR_LINKS.map((navbar_link) => {
-// 					return <NavbarLink key={navbar_link.label} navbar_link={navbar_link} />
-// 				})}
-// 				{user ? <LoggedInSection user={user} /> : <LoggedOutSection />}
-// 			</div>
-// 		</nav>
-// 	);
-// }
+function LoggedInSection({ user, onLogoutButtonClick }: LoggedInSectionProps) {
+	return (
+		<div className="flex gap-4 items-center">
+			<a href="/add" className="flex gap-2 items-center h-12 px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600">
+				<Plus className="h-6 w-6 stroke-white" />
+				<span>Add Project</span>
+			</a>
+			<a href={create_profile_page_link(user.username)} className="rounded-full w-8 h-8">
+				<img className="rounded-full w-full h-full" src={user.github_avatar_url} alt={`${user.username}'s avatar`} />
+			</a>
+            <button className="text-blue-700 bg-transparent p-0 hover:underline hover:cursor-pointer">Log Out</button>
+		</div>
+	)
+}
 
-// interface LoggedInSectionProps {
-// 	user: User;
-// }
-
-// function LoggedInSection({ user }: LoggedInSectionProps) {
-// 	return (
-// 		<div className="flex gap-4 items-center">
-// 			<a href="/add" className="flex gap-2 items-center h-12 px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600">
-// 				<Plus className="h-6 w-6 stroke-white" />
-// 				<span>Add Project</span>
-// 			</a>
-// 			<a href={create_username_link(user.whois_username)} className="rounded-full w-8 h-8">
-// 				<img className="rounded-full w-full h-full" src={user.avatar_img_url} alt={`${user.github_username}'s avatar`} />
-// 			</a>
-// 		</div>
-// 	)
-// }
-
-// function LoggedOutSection() {
-// 	return (
-// 		<div className="flex gap-4 items-center">
-// 			<a href="/login" className="h-10 flex justify-center items-center px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600">
-// 				Log In
-// 			</a>
-// 			<a href="/signup" className="text-blue-700 hover:underline hover:cursor-pointer">
-// 				Sign Up
-// 			</a>
-// 		</div>
-// 	);
-// }
+function LoggedOutSection() {
+	return (
+		<div className="flex gap-4 items-center">
+			<a href="/login" className="h-10 flex justify-center items-center px-4 rounded bg-blue-700 font-medium text-white hover:bg-blue-600">
+				Log In
+			</a>
+			<a href="/signup" className="text-blue-700 hover:underline hover:cursor-pointer">
+				Sign Up
+			</a>
+		</div>
+	);
+}
