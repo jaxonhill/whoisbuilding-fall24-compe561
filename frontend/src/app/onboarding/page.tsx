6361311b73a12b997be9e5c6c5a6d8112265d2d9
mhayescs19 <mhayescs19@gmail.com>
2024-12-08 14:54:07 -0800

    update multi layer schema validation with apis

--- frontend/src/app/onboarding/page.tsx
+++ frontend/src/app/onboarding/page.tsx
@@ -15,24 +15,103 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/features/auth/context/auth-context";
+import { fetchUserFieldValidation } from "@/lib/api/user";
+import { UniqueUserFields } from "@/types/db-types";
+import { checkIfUsernameExistsOnGitHub } from "@/lib/api/github";
 
 export default function OnboardingPage() {
-    return (
-        <div className="flex flex-col w-full gap-16 mt-16">
-            <div className="w-full flex flex-col gap-4">
-                <h1 className="text-3xl font-medium">Welcome to whoisbuilding!</h1>
-                <p className="text-slate-700 text-xl">Before you can use the site, please fill out the additional information below.</p>
-            </div>
-            <OnboardingForm />
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full gap-16 mt-16">
+      <div className="w-full flex flex-col gap-4">
+        <h1 className="text-3xl font-medium">Welcome to whoisbuilding!</h1>
+        <p className="text-slate-700 text-xl">
+          Before you can use the site, please fill out the additional
+          information below.
+        </p>
+      </div>
+      <OnboardingForm />
+    </div>
+  );
 }
 
 const schema = z.object({
-  first_name: z.string().min(1, "First name is required"),
-  last_name: z.string().min(1, "Last name is required"),
-  username: z.string().min(1, "Username is required"),
-  github: z.string().min(1, "GitHub username is required"),
+  first_name: z.string().superRefine((value, ctx) => {
+    if (value.trim().length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "First name is required",
+      });
+    } else if (value.trim().length < 2) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "First name must be at least 2 characters",
+      });
+    }
+  }),
+  last_name: z.string().superRefine((value, ctx) => {
+    if (value.trim().length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "First name is required",
+      });
+    } else if (value.trim().length < 2) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "First name must be at least 2 characters",
+      });
+    }
+  }),
+  username: z
+    .string()
+    .min(1, "Username is required")
+    .refine(async (username) => {
+      // validate the field by checking the db for existence
+      const fieldExists = await fetchUserFieldValidation(
+        UniqueUserFields.USERNAME,
+        username
+      );
+      return !fieldExists;
+    }, "Username already registered"),
+  github: z
+    .string()
+    .superRefine(async (github_username, ctx) => {
+      if (github_username.trim().length === 0) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "GitHub username is required",
+        });
+      } else {
+        const github_username_registered_on_github =
+          await checkIfUsernameExistsOnGitHub(github_username);
+
+        if (!github_username_registered_on_github) {
+          return ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Username provided is not valid on GitHub",
+          });
+        } else {
+          const github_username_exists_in_db = await fetchUserFieldValidation(
+            UniqueUserFields.GITHUB_USERNAME,
+            github_username
+          );
+
+          if (github_username_exists_in_db) {
+            return ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: "Username is already registered to an account",
+            });
+          }
+        }
+      }
+    })
+    .refine(async (github) => {
+      // validate the field by checking the db for existence
+      const fieldExists = await fetchUserFieldValidation(
+        UniqueUserFields.GITHUB_USERNAME,
+        github
+      );
+      return !fieldExists;
+    }, "GitHub username already registered"),
   linkedin: z.string().optional(),
   discord: z.string().optional(),
   bio: z.string().min(1, "Biography is required"),
@@ -51,7 +130,7 @@ export function OnboardingForm() {
       linkedin: "",
       discord: "",
       bio: "",
-    }
+    },
   });
 
   async function onSubmit(values: z.infer<typeof schema>) {
@@ -61,13 +140,18 @@ export function OnboardingForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full grid grid-cols-12 gap-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full grid grid-cols-12 gap-8"
+      >
         <FormField
           control={form.control}
           name="first_name"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">First Name *</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                First Name *
+              </FormLabel>
               <FormControl>
                 <Input placeholder="Enter first name" type="text" {...field} />
               </FormControl>
@@ -81,7 +165,9 @@ export function OnboardingForm() {
           name="last_name"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">Last Name *</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                Last Name *
+              </FormLabel>
               <FormControl>
                 <Input placeholder="Enter last name" type="text" {...field} />
               </FormControl>
@@ -95,7 +181,9 @@ export function OnboardingForm() {
           name="username"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">Username *</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                Username *
+              </FormLabel>
               <FormControl>
                 <Input placeholder="Enter username" type="text" {...field} />
               </FormControl>
@@ -109,9 +197,15 @@ export function OnboardingForm() {
           name="github"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">Github *</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                GitHub *
+              </FormLabel>
               <FormControl>
-                <Input placeholder="Enter Github username" type="text" {...field} />
+                <Input
+                  placeholder="Enter GitHub username"
+                  type="text"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -123,9 +217,15 @@ export function OnboardingForm() {
           name="linkedin"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">LinkedIn</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                LinkedIn
+              </FormLabel>
               <FormControl>
-                <Input placeholder="Enter LinkedIn profile URL" type="text" {...field} />
+                <Input
+                  placeholder="Enter LinkedIn profile URL"
+                  type="text"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -137,9 +237,15 @@ export function OnboardingForm() {
           name="discord"
           render={({ field }) => (
             <FormItem className="col-span-4">
-              <FormLabel className="font-medium text-base text-slate-950">Discord</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                Discord
+              </FormLabel>
               <FormControl>
-                <Input placeholder="Enter Discord username" type="text" {...field} />
+                <Input
+                  placeholder="Enter Discord username"
+                  type="text"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -151,12 +257,14 @@ export function OnboardingForm() {
           name="bio"
           render={({ field }) => (
             <FormItem className="col-span-8">
-              <FormLabel className="font-medium text-base text-slate-950">Biography *</FormLabel>
+              <FormLabel className="font-medium text-base text-slate-950">
+                Biography *
+              </FormLabel>
               <FormControl>
-                <Input 
-                  placeholder="Tell us about yourself! You can use emojis too 💯" 
-                  type="text" 
-                  {...field} 
+                <Input
+                  placeholder="Tell us about yourself! You can use emojis too 💯"
+                  type="text"
+                  {...field}
                 />
               </FormControl>
               <FormMessage />
@@ -165,11 +273,13 @@ export function OnboardingForm() {
         />
 
         <Button
-            onClick={() => {}}
-            disabled={!form.formState.isValid}
-            className="col-span-4 w-full self-end p-0 items-center bg-blue-700 h-12 hover:bg-blue-600 disabled:bg-slate-300"
+          onClick={() => {}}
+          //disabled={!form.formState.isValid}
+          className="col-span-4 w-full self-end p-0 items-center bg-blue-700 h-12 hover:bg-blue-600 disabled:bg-slate-300"
         >
-            <span className="text-white font-medium flex-shrink-0 text-base">Create profile</span>
+          <span className="text-white font-medium flex-shrink-0 text-base">
+            Create profile
+          </span>
         </Button>
       </form>
     </Form>
