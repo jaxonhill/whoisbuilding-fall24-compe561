6787bf9e07cb5adbc499c31899fca15f813b2fce
mhayescs19 <mhayescs19@gmail.com>
2024-12-08 21:10:40 -0800

    update models to make onboarding fields nullable

--- backend/src/app/models.py
+++ backend/src/app/models.py
@@ -12,19 +12,20 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
     email = Column(String, unique=True, nullable=False)
-    username = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=True)
     hashed_password = Column(String, nullable=False)
-    github_username = Column(String, unique=True, nullable=False)
-    profile_image_url = Column(String, nullable=False)
+    github_username = Column(String, unique=True, nullable=True)
+    profile_image_url = Column(String, nullable=True)
     linkedin = Column(String, nullable=True)
     discord = Column(String, nullable=True)
-    biography = Column(String(BIO_MAX_CHAR_LENGTH), nullable=False)
+    biography = Column(String(BIO_MAX_CHAR_LENGTH), nullable=True)
     expertise = Column(String, nullable=True)
     created_at = Column(DateTime, nullable=False)
     disabled = Column(Boolean, nullable=False)
+    is_onboarding_complete = Column(Boolean, nullable=False)
 
     projects = relationship("Project", back_populates="owner")
 
