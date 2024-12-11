0ba5d207b709e7204f390e4c70d6cb9d4c6212a0
Jaxon Hill <jaxoncharleshill@gmail.com>
2024-12-11 14:10:32 -0800

    Add function to search users by substring

--- frontend/src/lib/api/users.ts
+++ frontend/src/lib/api/users.ts
@@ -1,7 +1,17 @@
-import { User } from "@/types/db-types";
+import { User, UserDisplay } from "@/types/db-types";
 import { API_BASE_URL } from "./locals";
 
 export async function getUserByUsername(username: string) {
     const response = await fetch(`${API_BASE_URL}/users/${username}`);
     return response.json();
+}
+
+export async function searchUsers(searchText: string): Promise<UserDisplay[]> {
+  if (!searchText || searchText === "@") return [];
+  
+  const response = await fetch(`${API_BASE_URL}/users/search?q=${searchText}`);
+  if (!response.ok) {
+    throw new Error('Failed to search users');
+  }
+  return response.json();
 }
