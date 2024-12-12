0e0b89a2c21369b1185eb79d14269ff5119d5c00
mhayescs19 <mhayescs19@gmail.com>
2024-12-11 22:22:43 -0800

    add logo!

--- frontend/src/features/navbar/components/logo.tsx
+++ frontend/src/features/navbar/components/logo.tsx
@@ -1,14 +1,17 @@
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the font weights you need
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the font weights you need
 });
 
 export default function Logo() {
-	return (
-		<a href="/" className={`${poppins.className} font-normal text-xl`}>
-			whoisbuilding
-		</a>
-	);
+  return (
+    <div className="flex flex-row gap-2 items-center">
+      <img src="/whoisbuilding-blue.png" alt="logo" width={40} />
+      <a href="/" className={`${poppins.className} font-normal text-xl`}>
+        whoisbuilding
+      </a>
+    </div>
+  );
 }
